e27224dfaca3199cc25fba812f0dadc85e430531
alfredlucero <alfred.lucero@ucla.edu>
2018-10-10 22:27:21 -0700

    Starting up boilerplate for gatsby blog

---
+++ alfienityblog/src/pages/about.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default () => (
+  <div style={{ color: "#595959" }}>
+    <h1>About alfienitylucero</h1>
+    <p>Frontend Software Engineer II at SendGrid</p>
+  </div>
+);
---
+++ alfienityblog/src/pages/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "gatsby";
+
+export default () => (
+  <div style={{ color: "#595959" }}>
+    <h1>alfienitylucero</h1>
+    <p>
+      Exploring the full stack web development cosmos to alfienity and beyond~
+    </p>
+    <Link to="/about/">About</Link>
+  </div>
+);
